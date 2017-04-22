8254c5b14ee074ab8ed49ac69e8533651d7ef397
sravan <sravankumar.r3@gmail.com>
2017-04-22 15:34:49 +0530

    project setup

---
+++ src/main/java/com/sravan/mark5/Mark5AppApplication.java
@@ -0,0 +1,12 @@
+package com.sravan.mark5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mark5AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mark5AppApplication.class, args);
+	}
+}
